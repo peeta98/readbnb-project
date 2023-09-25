aab8feb4834c3568b62af50cf14ea74da90b388f
Pedro Moreira <pedromnmoreira98@gmail.com>
2023-09-25 13:37:14 +0000

    add validations to models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  has_many :reviews
+  has_many :user_books
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  has_many :user_books
+  has_many :reviews
+
+  validates :username, :gender, :age, presence: true
+  validates :username, uniqueness: true
+  validates :gender, inclusion: { in: %w[Male Female Other] }
+  validates :age, comparison: { greater_than_or_equal_to: 18 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
