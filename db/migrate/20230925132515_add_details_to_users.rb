bc24ccd123d549d1961c99ebe5a2231dc477f49e
Pedro Moreira <pedromnmoreira98@gmail.com>
2023-09-25 13:25:52 +0000

    add more details to user model

---
+++ db/migrate/20230925132515_add_details_to_users.rb
@@ -0,0 +1,7 @@
+class AddDetailsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :gender, :string
+    add_column :users, :age, :integer
+  end
+end
