032c3eadab057f21898f5e278ca3bb496878ab9e
Pedro Moreira <pedromnmoreira98@gmail.com>
2023-09-26 14:14:44 +0000

    Home page complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @action_books = Book.search_by_genre("Action")
+    @terror_books = Book.search_by_genre("Terror")
+    @comedy_books = Book.search_by_genre("Comedy")
+    @drama_books = Book.search_by_genre("Drama")
+    @fiction_books = Book.search_by_genre("Fiction")
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :user_books
+
+  def search_by_genre(genre)
+    return Book.where(genre:)
+  end
 end
